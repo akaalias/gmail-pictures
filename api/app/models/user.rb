d03d9d824262c32c11fdd76301e026500d23dc27
Alexis Rondeau <alexis.rondeau@gmail.com>
2015-03-26 20:46:23 -0400

    Add Mailchimp in production

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ActiveRecord::Base
 
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+
+  after_create :register_with_mailchimp
+
+  def register_with_mailchimp
+    unless Rails.env == "test" or Rails.env == "development"
+      gb = Gibbon::API.new(Rails.application.secrets.mailchimp_api_key)
+      gb.lists.subscribe({:id => "ef87dfc9a8", :email => {:email => self.email}, :merge_vars => {}, :double_optin => false})
+    end
+  end
 end
--- api/spec/requests/api/v1/users_spec.rb
+++ api/spec/requests/api/v1/users_spec.rb
@@ -3,10 +3,9 @@ require 'rails_helper'
 describe 'Users API', type: :request do
 
   describe "CREATE /users/" do
-    
     context "with valid attributes" do
       it 'creates a new user' do
-        post "/api/v1/users/", user: {email: "test@email.com", password: "password" }
+        post "/api/v1/users/", user: {email: "alexis.sample#{rand(1000)}@gmail.com", password: "password" }
         expect(response).to have_http_status(:success)
         puts response.body
         json = JSON.parse(response.body)
